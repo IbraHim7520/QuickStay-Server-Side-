d2e6d570dd7c12a6ffc2d213bd7267f999e4e82c
ibrahim752006 <mdibrahim752006@gmail.com>
2025-06-15 03:35:43 -0700

    Created a api to to post review in db from client side

--- index.js
+++ index.js
@@ -27,105 +27,131 @@ async function run() {
         const Booked_Room_Collection = database.collection('Booked_Rooms')
 
         app.get("/roomDetails", async (req, res) => { //Get Highest Rating 6 Room Details
-            const RoomsInfo = await RoomCollection.find().sort({roomRating: -1}).limit(6).toArray();
+            const RoomsInfo = await RoomCollection.find().sort({ roomRating: -1 }).limit(6).toArray();
             res.send(RoomsInfo);
         })
-        app.get('/getRooms', async(req, res)=>{
+        app.get('/getRooms', async (req, res) => {
             const roomData = await RoomCollection.find().toArray()
             res.send(roomData);
         })
-        app.get("/get_oneroom/:roomID", async(req , res)=>{
+        app.get("/get_oneroom/:roomID", async (req, res) => {
             const id = req.params.roomID;
-            const filter = {_id: new ObjectId(id)};
+            const filter = { _id: new ObjectId(id) };
 
             const result = await RoomCollection.findOne(filter);
             res.send(result);
 
         })
-        app.patch( '/room_booking/:id', async(req , res)=>{
+        app.patch('/room_booking/:id', async (req, res) => {
             const id = req.params.id;
-            const {Date , RoomID , BookedBy} = req.body;
-            const Room = await RoomCollection.findOne({_id: new ObjectId(RoomID)})
+            const { Date, RoomID, BookedBy } = req.body;
+            const Room = await RoomCollection.findOne({ _id: new ObjectId(RoomID) })
             const Booked_Room_Doc = {
                 Date,
                 RoomID,
                 BookedBy
             }
+            const setBookedBy = {
+                $addToSet: {
+                    BookedBy: BookedBy
+                }
+            }
+            const updateResult = await RoomCollection.updateOne(Room, setBookedBy);
             const db_response = await Booked_Room_Collection.insertOne(Booked_Room_Doc);
-            if (db_response.insertedId){
+            if (db_response.insertedId) {
                 const updte = {
-                    $set:{
-                        Booked : true
+                    $set: {
+                        Booked: true
                     }
                 }
                 const response = RoomCollection.updateOne(Room, updte);
-                if(response){
+                if (response) {
                     res.send(response)
                 }
             }
-        } )
+        })
 
-        app.get(`/get_booked_room/:email`, async(req , res)=>{
-            const {email} =  req.params;
-            const data_filter = {BookedBy: email};
+        app.get(`/get_booked_room/:email`, async (req, res) => {
+            const { email } = req.params;
+            const data_filter = { BookedBy: email };
             const datas = await Booked_Room_Collection.find(data_filter).toArray()
-        for (const data of datas){
-            const id = data.RoomID;
-            const roomInfo = await RoomCollection.findOne({_id: new ObjectId(id)});
+            for (const data of datas) {
+                const id = data.RoomID;
+                const roomInfo = await RoomCollection.findOne({ _id: new ObjectId(id) });
 
-            data.name = roomInfo.name
-            data.price = roomInfo.pricePerNight
-            data.image = roomInfo.image
-            data.address = roomInfo.hotelLocation
-            data.hotelName = roomInfo.hotelName
-        }
-        res.send(datas);
+                data.name = roomInfo.name
+                data.price = roomInfo.pricePerNight
+                data.image = roomInfo.image
+                data.address = roomInfo.hotelLocation
+                data.hotelName = roomInfo.hotelName
+            }
+            res.send(datas);
         })
 
 
-        app.delete("/cancel_booking/:id", async(req , res)=>{
+        app.delete("/cancel_booking/:id", async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)};
+            const filter = { _id: new ObjectId(id) };
             const document = await Booked_Room_Collection.findOne(filter);
             const roomid = document?.RoomID
             if (!roomid) {
-            return res.status(400).send({ error: "RoomID not found in booking." });
-                }
+                return res.status(400).send({ error: "RoomID not found in booking." });
+            }
             const result = await Booked_Room_Collection.deleteOne(filter);
-            if (result.deletedCount == 1){
-                const filter = {_id : new ObjectId(roomid)};
+            if (result.deletedCount == 1) {
+                const filter = { _id: new ObjectId(roomid) };
                 const updateDoc = {
-                    $set:{
+                    $set: {
                         Booked: false
                     }
                 }
-                const update_confirm = await RoomCollection.updateOne(filter , updateDoc);
-                if(update_confirm.modifiedCount){
+                const update_confirm = await RoomCollection.updateOne(filter, updateDoc);
+                if (update_confirm.modifiedCount) {
                     res.send(result)
                 }
             }
 
         })
 
-        app.get('/filtered_data/:filter', async(req , res)=>{
+        app.get('/filtered_data/:filter', async (req, res) => {
             const filter_text = req.params.filter;
-            if( filter_text.includes('price high to low')){
-                const pHightoLow = await RoomCollection.find().sort( {pricePerNight: -1}).toArray()
+            if (filter_text.includes('price high to low')) {
+                const pHightoLow = await RoomCollection.find().sort({ pricePerNight: -1 }).toArray()
                 res.send(pHightoLow);
-            }else if (filter_text.includes("price low to high")){
-                const pLowtoHigh = await RoomCollection.find().sort({pricePerNight: 1}).toArray();
+            } else if (filter_text.includes("price low to high")) {
+                const pLowtoHigh = await RoomCollection.find().sort({ pricePerNight: 1 }).toArray();
                 res.send(pLowtoHigh)
-                
-            }else if (filter_text.includes('rating high to low')){
-                const rHightoLow = await RoomCollection.find().sort({roomRating: -1}).toArray();
+
+            } else if (filter_text.includes('rating high to low')) {
+                const rHightoLow = await RoomCollection.find().sort({ roomRating: -1 }).toArray();
                 res.send(rHightoLow)
-               
-            }else if (filter_text.includes('ratng low to high')){
-                const rLowtoHigh = await RoomCollection.find().sort({roomRating:1}).toArray();
+
+            } else if (filter_text.includes('ratng low to high')) {
+                const rLowtoHigh = await RoomCollection.find().sort({ roomRating: 1 }).toArray();
                 res.send(rLowtoHigh);
-                
+
             }
-        } )
+        })
+
+        app.patch('/add-reviews', async (req, res) => {
+            const data = req.body
+            data.Time = Date.now()
+            console.log(data)
+            const Id = data.RoomID;
+            console.log(Id);
+            const filter = { _id: new ObjectId(Id) };
+        
+            const updateReview = {
+                $push: {
+                    reviews: {
+                        ...data,
+                    }
+                }
+                // Or use $addToSet if you want to prevent duplicates
+            };
+            const updateReviewResult = await RoomCollection.updateOne(filter, updateReview)
+            res.send(updateReviewResult)
+        })
 
         console.log("Connected");
     } finally {
